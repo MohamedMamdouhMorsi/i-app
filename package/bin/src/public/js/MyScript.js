cafe255585c23b9b7f2504b1079ffa868747ea9c
mwn <info@m-w-n.com>
2023-09-23 07:56:49 +0200

    create i-app CLI  js

---
+++ package/bin/src/public/js/MyScript.js
@@ -0,0 +1,18 @@
+/** 
+ * i-app
+ * your script run here
+ * link your script with any
+ * .app file by key 
+ * script:{
+ *      n:'MyScript'
+ *      v:{some data to your function}
+ * }
+ *  
+*/
+
+const MyScript = (v,[_,app])=>{
+    // v   is your income data from .app
+    // _   is your tools object 
+    // app is your app info
+    // happy Coding 
+}
